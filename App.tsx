3b174e8a3383fa8f7f0e7d5f58198c4e3fc45209
Dmitry Fokin <fokind1@yandex.ru>
2021-06-17 21:04:58 +0300

    навигатор

--- App.tsx
+++ App.tsx
@@ -1,25 +1,19 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { ActionBar, Button } from './components';
+import { Button } from './components';
 
 export default function App() {
   return (
-    <>
-      <ActionBar
-        actions={
-          <>
-            <Button>Button</Button>
-          </>
-        }
-        title="Page Title"
-      />
+    <NavigationContainer>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Button>Кнопка</Button>
         <StatusBar style="auto" />
       </View>
-    </>
+    </NavigationContainer>
   );
 }
 
--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,1 @@
-export { default as ActionBar } from './ActionBar/ActionBar';
 export { default as Button } from './Button/Button';
