ce65bed367bbfe7000cc947ddac11c13d2301d22
Dmitry Fokin <fokind1@yandex.ru>
2021-06-17 16:40:28 +0300

    button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,39 +3,40 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export interface IButtonProps {
   children?: string;
+  compact?: boolean;
   glyph?: string;
   option?: string;
   onClick?: () => void;
 }
 
-const Button: React.FC<IButtonProps> = ({ children, onClick }) => {
+const Button: React.FC<IButtonProps> = ({ children, compact, onClick }) => {
+  const styles = StyleSheet.create({
+    buttonText: {
+      fontSize: 14,
+      lineHeight: 19.6,
+      color: '#32363a',
+      fontWeight: '400',
+    },
+    button: {
+      paddingHorizontal: 9,
+      verticalAlign: 'middle',
+      whiteSpace: 'nowrap',
+      borderRadius: 4,
+      height: compact ? 26 : 36,
+      borderStyle: 'solid',
+      borderWidth: 1,
+      color: '#0854a0',
+      borderColor: '#0854a0',
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+    },
+  });
+
   return (
     <TouchableOpacity onPress={onClick} style={styles.button}>
       <Text style={styles.buttonText}>{children}</Text>
     </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  buttonText: {
-    fontSize: 14,
-    lineHeight: 19.6,
-    color: '#32363a',
-    fontWeight: '400',
-  },
-  button: {
-    paddingHorizontal: 9,
-    verticalAlign: 'middle',
-    whiteSpace: 'nowrap',
-    borderRadius: 4,
-    height: 36,
-    borderStyle: 'solid',
-    borderWidth: 1,
-    color: '#0854a0',
-    borderColor: '#0854a0',
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-});
 
 export default Button;
